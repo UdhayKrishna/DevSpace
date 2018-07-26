bf79b9cd36eb217201a6d435e67cd916bfb5f230
UdhayKrishna <udhayakumarkrishnamoorthy@gmail.com>
2018-07-26 11:05:43 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -11,25 +11,24 @@ if($method == 'POST'){
 
 	switch ($text) {
 		case 'hi':
-			$speech = "Hi, Nice to meet you";
+			$textToSpeech = "Hi, Nice to meet you";
 			break;
 
 		case 'bye':
-			$speech = "Bye, good night";
+			$textToSpeech = "Bye, good night";
 			break;
 
 		case 'anything':
-			$speech = "Yes, you can type anything here.";
+			$textToSpeech = "Yes, you can type anything here.";
 			break;
 		
 		default:
-			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			$textToSpeech = "Sorry, I didnt get that. Please ask me something else.";
 			break;
 	}
 
 	$response = new \stdClass();
-	$response->speech = $speech;
-	$response->displayText = $speech;
+	$response->fulfillmentText = $textToSpeech;
 	$response->source = "webhook";
 	echo json_encode($response);
 }
